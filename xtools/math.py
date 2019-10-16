11d40e90d45aa740597ee00adf713416f9062712
xikasan <xika@xika.jp>
2019-10-17 00:26:47 +0900

    dev round in math

--- xtools/math.py
+++ xtools/math.py
@@ -12,3 +12,7 @@ def r2d(r):
     if hasattr(r, "__len__"):
         return [r2d(re) for re in r]
     return r * 180 / np.pi
+
+def round(x, d=0):
+    p = 10 ** d
+    return float(np.floor((x * p) + np.copysign(0.5, x))) / p
