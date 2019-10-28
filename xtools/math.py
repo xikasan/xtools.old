9fe80fcb39a2910faabb43c203a53bd50884408d
xikasan <xika@xika.jp>
2019-10-28 13:04:19 +0900

    [feat] add type specific convert r2d d2r

--- xtools/math.py
+++ xtools/math.py
@@ -5,13 +5,20 @@ import numpy as np
 
 def d2r(d):
     if hasattr(d, "__len__"):
-        return [d2r(de) for de in d]
+        if isinstance(d, list):
+            return [d2r(de) for de in d]
+        if isinstance(d, tuple):
+            return (d2r(de) for de in d)
+    print(d)
     return d * np.pi / 180
 
 
 def r2d(r):
     if hasattr(r, "__len__"):
-        return [r2d(re) for re in r]
+        if isinstance(r, list):
+            return [r2d(re) for re in r]
+        if isinstance(r, tuple):
+            return (r2d(re) for re in r)
     return r * 180 / np.pi
 
 
