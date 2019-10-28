1456d2dcdb03fdf2778492400b79bbd5a2ee39c0
xikasan <xika@xika.jp>
2019-10-28 13:05:18 +0900

    [style] add blank line to follow PEP8

--- xtools/path.py
+++ xtools/path.py
@@ -24,16 +24,19 @@ def go_to_root(root_name=None, verbose=False):
 def join(*args):
     return os.path.join(*args)
 
+
 def mkdirs(path, exist_ok=False):
     if not exist_ok and not os.path.exists(path):
         raise FileExistsError
     os.mkdirs(path, exist_ok)
+
 
 def generate_time_dir_path(path=None, format="%Y.%m.%d.%H%M%S"):
     if path is None:
         path = os.getcwd()
     now = datetime.datetime.now().strftime(format)
     return join(path, now)
+
 
 def make_dirs_current_time(path=None, format="%Y.%m.%d.%H%M%S", exist_ok=True):
     path = generate_time_dir_path(path)
