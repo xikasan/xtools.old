1eaf3d31030044d4fc4cb823621e00f39b810455
xikasan <xika0907@gmail.com>
2020-01-28 23:05:36 +0900

    [dev] config class

--- xtools/config.py
+++ xtools/config.py
@@ -8,3 +8,16 @@ def load_config(file):
     with open(file) as fp:
         conf = yaml.safe_load(fp)
     return conf
+
+
+class Config:
+
+    def __init__(self, config):
+        self._cf = config if isinstance(config, dict) else load_config(config)
+        self._retrieve(self._cf)
+
+    def _retrieve(self, cf):
+        for key, val in cf.items():
+            if isinstance(val, dict):
+                val = Config(val)
+            self.__setattr__(key, val)
