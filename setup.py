86bc560a0a9b9b3cc41aa3747f20735dfd167ac2
xikasan <xika0907@gmail.com>
2020-01-29 08:16:19 +0900

    version number up

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='xtools',
-    version='0.2.0',
+    version='0.2.1',
     description="xikasan's basic tool set",
     long_description=readme,
     author='xikasan',
